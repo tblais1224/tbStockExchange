92fcc3364581dca6b0d65f4a88e30ed1c4ec81cd
tblais1224 <thomasblais.student@careerdevs.com>
2019-11-11 21:23:35 -0500

    working on switching to mysql

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import os
 import requests
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, make_response, render_template, request, session
+from flask_mysqldb import MySQL
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
@@ -15,6 +16,10 @@ app = Flask(__name__)
 # export API_KEY=value
 
 # Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Ensure responses aren't cached
@@ -36,7 +41,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = MySQL(app)
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
